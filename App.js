49c6e400651db8a5992c5939b1154f95236d9b01
Phoenix214 <phoenix214@protonmail.com>
2020-11-27 12:16:06 -0800

    updated packages and app.js

--- App.js
+++ App.js
@@ -1,60 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-native-paper';
+import App from './src';
+import { theme } from './src/core/theme';
 
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+const Main = () => (
+  <Provider theme={theme}>
+    <App />
+  </Provider>
+);
 
-export default function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  return (
-    <View style={styles.container}>
-      <TextInput  
-        style={styles.inputText}
-        placeholder="Email..." 
-        placeholderTextColor="#003f5c"
-        onChangeText={text => setEmail(text)}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#003f5c',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#fb5b5a",
-    marginBottom:40
-  },
-  inputView: {
-    width:"80%",
-    backgroundColor:"#465881",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText: {
-    height:50,
-    color:"white"
-  },
-  forgot: {
-    color:"white",
-    fontSize:11
-  },
-  loginBtn: {
-    width:"80%",
-    backgroundColor:"#fb5b5a",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-});
+export default Main;
