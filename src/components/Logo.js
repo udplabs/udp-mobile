bfc05c90ae22a720caf9fcf753811cf4620dc52c
Phoenix214 <phoenix214@protonmail.com>
2021-01-27 03:36:55 -0500

    added remote logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,13 @@
 import React, { memo } from 'react';
 import { Image, StyleSheet } from 'react-native';
+import configFile from '../../samples.config';
 
-const Logo = () => (
-  <Image source={require('../assets/logo.png')} style={styles.image} />
-);
+const Logo = () => {
+  if(configFile.logoUrl) {
+    Image.prefetch(configFile.logoUrl);
+  }
+  return <Image source={configFile.logoUrl ? {uri: configFile.logoUrl} : require('../assets/logo.png')} style={styles.image} />
+};
 
 const styles = StyleSheet.create({
   image: {
